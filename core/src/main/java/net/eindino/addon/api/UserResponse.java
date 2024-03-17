b23e7bfe2ab17df35d42a3fb4d9214769144bdbb
Kevin Schulte <kevinschulte2002@gmail.com>
2024-03-17 01:07:44 +0100

    clean up code

--- core/src/main/java/net/eindino/addon/api/UserResponse.java
+++ core/src/main/java/net/eindino/addon/api/UserResponse.java
@@ -2,29 +2,18 @@ package net.eindino.addon.api;
 
 public class UserResponse {
 
+  private int id;
   private String name;
   private int coins;
   private int gold;
   private String locale;
 
-  public void setLocale(String locale) {
-    this.locale = locale;
+  public int getId() {
+    return id;
   }
 
   public String getLocale() {
     return locale;
-  }
-
-  public void setCoins(int coins) {
-    this.coins = coins;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public void setGold(int gold) {
-    this.gold = gold;
   }
 
   public int getCoins() {
